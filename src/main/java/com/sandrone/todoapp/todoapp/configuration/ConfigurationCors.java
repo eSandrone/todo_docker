5db2a9ea292ba3573b85928202c7dc7133ed6fae
eSandrone <e.sandrone.repo@gmail.com>
2022-07-04 11:40:34 +0200

    image update

--- src/main/java/com/sandrone/todoapp/todoapp/configuration/ConfigurationCors.java
+++ src/main/java/com/sandrone/todoapp/todoapp/configuration/ConfigurationCors.java
@@ -1,39 +1,16 @@
 package com.sandrone.todoapp.todoapp.configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ConfigurationCors implements Filter {
+@EnableWebMvc
+public class ConfigurationCors implements WebMvcConfigurer {
 
-    private final Logger log = LoggerFactory.getLogger(ConfigurationCors.class);
-
-    public ConfigurationCors() {
-        log.info("SimpleCORSFilter init");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException { }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpServletRequest request= (HttpServletRequest) servletRequest;
-
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Max-Age", "180");
-        chain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
-    public void destroy() { }
 
 }
